8726bdd3ed3b084fa8d13c4d42f4f6d364178d38
Johan Melin <toi@devtoi.eu>
2015-09-23 20:45:35 +0200

    Duplicated concurrency\/contention test for standard allocator for easier comparing between custom and standard

--- src/core/subsystem/testing/SSPoolThreadingTest.cpp
+++ src/core/subsystem/testing/SSPoolThreadingTest.cpp
@@ -10,16 +10,41 @@ int SSPoolThreadingTest::ID = -1;
 static const int l_NrOfThreads = 4;
 static const int l_AllocationSize = 16;
 static const std::string l_ConcurrencyProfileName = "ConcurrencyAllocationTest";
+static const std::string l_ConcurrencySTDAllocProfileName = "ConcurrencySTDAllocationTest";
+static const std::string l_ConcurrencyWithAllocatorParameterProfileName = "ConcurrencyWithAllocatorParameterProfileName";
 
 void ConcurrencyAllocationTest ( ) {
 	const int nrOfAllocations = TOI_TEMPLATED_POOL_ALLOCATOR_NR_OF_BLOCKS / l_NrOfThreads;
 	const int allocationSize = l_AllocationSize;
-	size_t* allocations[nrOfAllocations];
+	size_t* allocations[nrOfAllocations]; // If it crashes here it might be because the array is to big
 	for ( int i = 0; i < nrOfAllocations; ++i ) {
 		allocations[i] = (size_t*)poolAlloc( allocationSize );
 	}
 	for ( int i = 0; i < nrOfAllocations; ++i ) {
 		poolFree( allocationSize, allocations[i] );
+	}
+}
+
+void ConcurrencyAllocationTestSTDAlloc ( ) {
+	const int nrOfAllocations = TOI_TEMPLATED_POOL_ALLOCATOR_NR_OF_BLOCKS / l_NrOfThreads;
+	const int allocationSize = l_AllocationSize;
+	size_t* allocations[nrOfAllocations]; // If it crashes here it might be because the array is to big
+	for ( int i = 0; i < nrOfAllocations; ++i ) {
+		allocations[i] = (size_t*)malloc( allocationSize );
+	}
+	for ( int i = 0; i < nrOfAllocations; ++i ) {
+		free( allocations[i] );
+	}
+}
+
+void ConcurrencyAllocationTestWithAllocatorParameter(ToiTemplatedPoolAllocator<l_AllocationSize, TOI_TEMPLATED_POOL_ALLOCATOR_NR_OF_BLOCKS>* allocator ) {
+	const int nrOfAllocations = TOI_TEMPLATED_POOL_ALLOCATOR_NR_OF_BLOCKS / l_NrOfThreads;
+	size_t* allocations[nrOfAllocations];
+	for ( int i = 0; i < nrOfAllocations; ++i ) {
+		allocations[i] = (size_t*)allocator->allocate();
+	}
+	for ( int i = 0; i < nrOfAllocations; ++i ) {
+		allocator->deallocate( allocations[i] );
 	}
 }
 
@@ -35,11 +60,33 @@ void RunTests() {
 			threads[i].join();
 		}
 	}
+	{
+		std::thread threads[l_NrOfThreads];
+		PROFILE(AutoProfiler profile(l_ConcurrencySTDAllocProfileName, Profiler::PROFILER_CATEGORY_STANDARD));
+		for ( int i = 0; i < l_NrOfThreads; ++i ) {
+			threads[i] = std::thread( ConcurrencyAllocationTestSTDAlloc );
+		}
+		for ( int i = 0; i < l_NrOfThreads; ++i ) {
+			threads[i].join();
+		}
+	}
+	{
+		std::thread threads[l_NrOfThreads];
+		PROFILE(AutoProfiler profile(l_ConcurrencyWithAllocatorParameterProfileName, Profiler::PROFILER_CATEGORY_STANDARD));
+		for ( int i = 0; i < l_NrOfThreads; ++i ) {
+			threads[i] = std::thread( ConcurrencyAllocationTestWithAllocatorParameter, &GetPoolAllocator<l_AllocationSize>() );
+		}
+		for ( int i = 0; i < l_NrOfThreads; ++i ) {
+			threads[i].join();
+		}
+	}
 }
 
 void SSPoolThreadingTest::Startup( SubsystemCollection* const ) {
 	RunTests();
 	std::cout << l_ConcurrencyProfileName << ": " << g_Profiler.GetEntryLatestMilliseconds( l_ConcurrencyProfileName ) << "ms" << std::endl;
+	std::cout << l_ConcurrencyWithAllocatorParameterProfileName << ": " << g_Profiler.GetEntryLatestMilliseconds( l_ConcurrencyWithAllocatorParameterProfileName ) << "ms" << std::endl;
+	std::cout << l_ConcurrencySTDAllocProfileName << ": " << g_Profiler.GetEntryLatestMilliseconds( l_ConcurrencySTDAllocProfileName ) << "ms" << std::endl;
 }
 
 void SSPoolThreadingTest::Shutdown( SubsystemCollection* const subsystemCollection ) {
